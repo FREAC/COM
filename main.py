7de612c1e9eed1a54c218b820eb2093056bb0bc8
pgriffith <philip.griffith@gmail.com>
2018-12-10 17:07:04 -0500

    Added HERE class. Working on find function.

--- main.py
+++ main.py
@@ -2,19 +2,6 @@ import json
 import urllib.parse as up
 import requests
 
-here = 'https://geocoder.api.here.com/6.2/geocode.json'
-app_id = '3peEKduvqXuYDzZZnj0g'
-app_code = 'uur4uqOEz0zJZZWRr1kg1w'
-
-additional_data = 'IncludeMicroPointAddresses,true;PreserveUnitDesignators,true'
-address = '2039 N Meridian Rd APT 139 Tallahassee FL 32303'
-payload = {'app_id': app_id, 'app_code': app_code, 'searchtext': up.quote_plus(address),
-           'country': 'USA', 'state': 'US:FL', 'additionaldata': additional_data}
-
-# r = requests.get(here, params=payload)
-# print(r.status_code)
-# print(r.url)
-#
 # with open('response.json', 'w') as f:
 #     json.dump(r.json(), f)
 
@@ -22,7 +9,45 @@ with open('response.json', 'r') as f:
     r = json.load(f)
 
 print(r)
-print(len(r['Response']['View'][0]['Result']))
+
+
 print(r['Response']['View'][0]['Result'][0]['Location']['NavigationPosition'])
 print(r['Response']['View'][0]['Result'][0]['Location']['Related'][0]['Location']['DisplayPosition'])
+
+
+class HERE:
+
+    def __init__(self):
+
+        self.here = 'https://geocoder.api.here.com/6.2/geocode.json'
+        self.app_id = '3peEKduvqXuYDzZZnj0g'
+        self.app_code = 'uur4uqOEz0zJZZWRr1kg1w'
+        self.additional_data = 'IncludeMicroPointAddresses,true;PreserveUnitDesignators,true'
+
+    def find(self, address):
+
+        payload = {'app_id': self.app_id, 'app_code': self.app_code, 'searchtext': up.quote_plus(address),
+                   'country': 'USA', 'state': 'US:FL', 'additionaldata': self.additional_data}
+        r = requests.get(self.here, params=payload)
+        r.raise_for_status()
+        results = r['Response']['View'][0]['Result']
+        for result in results:
+            relevance = result['Relevance']
+            matchQuality = result['MatchQuality']
+            if 'Related' in result:
+                for related in result['Related']:
+                    continue
+            latlon = result['Location']['DisplayPosition']
+            return_address = result['Address']
+
+
+
+
+
+if __name__ == '__main__':
+
+    addr = '2039 N Meridian Rd APT 139 Tallahassee FL 32303'
+
+    h = HERE()
+
 
