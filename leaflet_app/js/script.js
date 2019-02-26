a71ad7c66ac3dba68d22a68655626756debda9fc
clintonlunn <cpl09c@gmail.com>
2019-02-26 14:50:32 -0500

    add tooltips and comments for address search bar

--- leaflet_app/js/script.js
+++ leaflet_app/js/script.js
@@ -213,8 +213,12 @@ function geocodeAddress(address) {
     const queryString = params + address;
     $.get(url, queryString, function (data) {
         if (data.candidates.length !== 0) {
+            // if the is-invalid class is present, remove it
             if ($('#geocoder-input').hasClass('is-invalid')) {
                 $('#geocoder-input').removeClass('is-invalid');
+                // hide invalide address message
+                $('.invalid-feedback').hide();
+
             }
             const coords = data.candidates[0].location;
             const location = {
@@ -223,9 +227,10 @@ function geocodeAddress(address) {
             };
             geocodePlaceMarkersOnMap(location);
         } else {
-            // change color of text to bootstrap warning
+            // change color of text to bootstrap is-invalid class to show user that their input was invalid
             $('#geocoder-input').addClass("is-invalid");
-            console.log('changing color')
+            // add invalid address message
+            $('.invalid-feedback').show();
         }
 
     });
