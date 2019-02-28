a636f5f3ff7938edb26101436363c370ded16e17
clintonlunn <cpl09c@gmail.com>
2019-02-28 12:50:43 -0500

    rudimentary filtering, not dependant on other checkbox

--- leaflet_app/js/script.js
+++ leaflet_app/js/script.js
@@ -1,5 +1,7 @@
 // We'll append our markers to this global variable
-const json_group = new L.FeatureGroup();
+const json_group = new L.FeatureGroup([]);
+//This is our selection group
+const selection_group = new L.FeatureGroup();
 // This is the circle on the map that will be determine how many markers are around
 let circle;
 // this is the icon in the middle of the circle
@@ -379,107 +381,132 @@ function markerStyle(radius, fillColor, color, weight, fillOpacity) {
 
 // This loops through the data in our JSON file
 // And puts it on the map
+function markerLogic(num) {
+    const dataLat = num['Latitude'];
+    const dataLong = num['Longitude'];
 
+    const popup = L.popup()
+        .setContent(
+            `
+        <p><strong>Company Name: </strong> ${num['CompanyNam']}</p>
+        <p><strong>Company Link: </strong><a href='https://www.google.com/search?q= + ${num['CountyName']}' target="_blank">${num['CountyName']}</a></p>
+        `
+        );
 
-$.get("./js/data/group_care.json", function (json_data) {
+    // Add to our marker
+    const marker_location = new L.LatLng(dataLat, dataLong);
+    const layer_marker = L.circleMarker(marker_location, markerStyle(4, "#ED9118", "#FFFFFF", 1, num['CountyCode'] / 100))
+        .bindPopup(popup);
 
-    _.each(json_data, function (num) {
-        const dataLat = num['Latitude'];
-        const dataLong = num['Longitude'];
+    // Build the data
+    layer_marker.data = {
+        'CompanyName': num['CompanyNam'],
+        'CountyName': num['CountyName'],
+        'CountyCode': num['CountyCode'],
+        'CountyNum': num['CountyNumb']
+    };
 
-        const popup = L.popup()
-            .setContent(
-                `
-                <p><strong>Company Name: </strong> ${num['CompanyNam']}</p>
-                <p><strong>Company Link: </strong><a href='https://www.google.com/search?q= + ${num['CountyName']}' target="_blank">${num['CountyName']}</a></p>
-                `
-            );
+    // Add events to marker
+    layer_marker.on({
+        // What happens when mouse hovers markers
+        mouseover: function (e) {
+            // if the moused over marker is not red, 
+            // display the mouseover color change
+            if (e.target.options.fillColor !== "#FF0000") { // marker is not already red (clicked)
+                const layer_marker = e.target;
+                layer_marker.setStyle(markerStyle(4, "#2BBED8", "#2BBED8", 1, 1));
+            }
 
-        // Add to our marker
-        const marker_location = new L.LatLng(dataLat, dataLong);
-        const layer_marker = L.circleMarker(marker_location, markerStyle(4, "#ED9118", "#FFFFFF", 1, num['CountyCode'] / 100))
-            .bindPopup(popup);
+        },
+        // What happens when mouse leaves the marker
+        mouseout: function (e) {
+            // if the marker is not read, change the color back to original marker color
+            // otherwise, leave it be
+            if (e.target.options.fillColor !== "#FF0000") { // marker is not already red (clicked)
+                const layer_marker = e.target;
+                layer_marker.setStyle(markerStyle(4, "#ED9118", "#FFFFFF", 1, .8));
 
-        // Build the data
-        layer_marker.data = {
-            'CompanyName': num['CompanyNam'],
-            'CountyName': num['CountyName'],
-            'CountyCode': num['CountyCode'],
-            'CountyNum': num['CountyNumb']
-        };
+            }
+        },
+        // What happens when the marker is clicked
+        click: function (e) {
+            // if there is no click marker yet, assign one
+            if (selection_marker === undefined) {
+                selection_marker = e.target;
+                selection_marker.setStyle(markerStyle(4, "#FF0000", "#FF0000", 1, .8));
+            } else { // if there is a click marker already
+                // assign old marker back to original color
+                selection_marker.setStyle(markerStyle(4, "#ED9118", "#FFFFFF", 1, .8));
 
-        // Add events to marker
-        layer_marker.on({
-            // What happens when mouse hovers markers
-            mouseover: function (e) {
-                // if the moused over marker is not red, 
-                // display the mouseover color change
-                if (e.target.options.fillColor !== "#FF0000") { // marker is not already red (clicked)
-                    const layer_marker = e.target;
-                    layer_marker.setStyle(markerStyle(4, "#2BBED8", "#2BBED8", 1, 1));
-                }
-
-            },
-            // What happens when mouse leaves the marker
-            mouseout: function (e) {
-                // if the marker is not read, change the color back to original marker color
-                // otherwise, leave it be
-                if (e.target.options.fillColor !== "#FF0000") { // marker is not already red (clicked)
-                    const layer_marker = e.target;
-                    layer_marker.setStyle(markerStyle(4, "#ED9118", "#FFFFFF", 1, .8));
-
-                }
-            },
-            // What happens when the marker is clicked
-            click: function (e) {
-                // if there is no click marker yet, assign one
-                if (selection_marker === undefined) {
-                    selection_marker = e.target;
-                    selection_marker.setStyle(markerStyle(4, "#FF0000", "#FF0000", 1, .8));
-                } else { // if there is a click marker already
-                    // assign old marker back to original color
-                    selection_marker.setStyle(markerStyle(4, "#ED9118", "#FFFFFF", 1, .8));
-
-                    // assign new marker to red
-                    console.log(e.target);
-                    selection_marker = e.target;
-                    selection_marker.setStyle(markerStyle(4, "#FF0000", "#FF0000", 1, .8));
-                }
-                // if a tabulator table is already active
-                if ($('#results-table').hasClass('tabulator')) {
-                    // get the data that is inside of it
-                    const data = table.getData();
-                    // loop through data to see if clicked feature matches
-                    for (let i in data) {
-                        // if we find a layer match, select it
-                        if (e.target.data.CompanyName === data[i].name) {
-                            // deselect previous row selection
-                            table.deselectRow();
-                            // select new row selection
-                            table.selectRow(i);
-                        }
+                // assign new marker to red
+                console.log(e.target);
+                selection_marker = e.target;
+                selection_marker.setStyle(markerStyle(4, "#FF0000", "#FF0000", 1, .8));
+            }
+            // if a tabulator table is already active
+            if ($('#results-table').hasClass('tabulator')) {
+                // get the data that is inside of it
+                const data = table.getData();
+                // loop through data to see if clicked feature matches
+                for (let i in data) {
+                    // if we find a layer match, select it
+                    if (e.target.data.CompanyName === data[i].name) {
+                        // deselect previous row selection
+                        table.deselectRow();
+                        // select new row selection
+                        table.selectRow(i);
                     }
                 }
             }
+        }
+    });
+    json_group.addLayer(layer_marker);
+
+    // Close for loop
+}
+
+function setup() {
+    $.get("./js/data/group_care.json", function (json_data) {
+
+        _.each(json_data, function (num) {
+            markerLogic(num)
+        }, this);
+
+    });
+}
+
+setup();
+
+async function filterFunction(e, filterCond) {
+    await json_group.clearLayers();
+    if (e.target.checked) {
+        // Get json data and search it
+        const json_data = await $.get("./js/data/group_care.json", function (json_data) {
+            _.each(json_data, function (num) {
+                if (num['CountyCode'] <= filterCond) {
+                    //console.log('yes');
+                    markerLogic(num);
+                }
+            }, this);
         });
-        json_group.addLayer(layer_marker);
-        // Close for loop
-    }, this);
+    } else {
+        setup();
+    }
+}
 
-    // alert for greater than 80 checkbox
-    const lessOrEqualTo80 = $('#lessOrEqualTo80');
 
-    greaterThan80.on('click', function () {
-        console.log('filter for features')
-    });
-    // alert for less than 60 checkbox
-    const lessOrEqualTo45 = $('#lessOrEqualTo45');
+// alert for greater than 80 checkbox
+const lessOrEqualTo80 = $('#lessOrEqualTo80');
 
-    lessOrEqualTo45.on('click', function () {
-        console.log('filter for features')
-    });
+lessOrEqualTo80.on('change', async function (e) {
+    filterFunction(e, 80);
 });
+// alert for less than 60 checkbox
+const lessOrEqualTo45 = $('#lessOrEqualTo45');
 
+lessOrEqualTo45.on('change', function (e) {
+    filterFunction(e, 45);
+});
 // Base map
 let basemap = L.tileLayer.provider('OpenStreetMap.Mapnik');
 
