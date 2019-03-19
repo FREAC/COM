a458c4710a86272c4c2eff882b43f2b8d1708302
pgriffith <philip.griffith@gmail.com>
2019-03-19 15:09:49 -0400

    What does this do 2?

---
+++ tryitout.py
@@ -0,0 +1,14 @@
+import ../HERE
+
+path = r'D:\Projects\COM\data\CPEIP_Unique_Sales_Locations.csv'
+addr = '415 N McKinley Street Little Rock AR 72205'
+
+HERE.Geocoder(path)
+
+
+
+# for address in addresses.data[:6]:
+#     print(address)
+# #     g.find(address)
+
+# g.find('Suite 610 415 N McKinley Street Little Rock AR 72205')
