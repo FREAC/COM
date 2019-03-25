83cc7d9b3fae82b8af894c0eeb9f116a12b135cc
clintonlunn <cpl09c@gmail.com>
2019-03-25 13:17:45 -0400

    make autocomplete into function

--- leaflet_app/js/autocomplete.js
+++ leaflet_app/js/autocomplete.js
@@ -1,26 +1,28 @@
-// Options for the autocomplete plugin
-const options = {
-    url: "./js/data/group_care.json",
-    // set multiple fields as searchable values by adding them to properties
-    getValue: function (element) {
-        return $(element).prop("CompanyNam"); // (how to add more fields)+ "<br>" + $(element).prop("CompleteSt");
-    },
-    list: {
-        match: {
-            enabled: true
+function setAutocomplete(json_data) { // Options for the autocomplete plugin
+    const options = {
+        // url: "./js/data/group_care.json",
+        data: json_data,
+        // set multiple fields as searchable values by adding them to properties
+        getValue: function (element) {
+            return $(element).prop("CompanyNam"); // (how to add more fields)+ "<br>" + $(element).prop("CompleteSt");
         },
-        onClickEvent: function () {
-            // when suggestion clicked, add company name to the search bar
-            var newvalue = jQuery("#geocoder-input").getSelectedItemData().CompanyNam;
-            jQuery("#geocoder-input").val(newvalue);
-        },
-        onChooseEvent: function () {
-            var index = $("#geocoder-input").getSelectedItemData().CompanyNam;
+        list: {
+            match: {
+                enabled: true
+            },
+            onClickEvent: function () {
+                // when suggestion clicked, add company name to the search bar
+                var newvalue = jQuery("#geocoder-input").getSelectedItemData().CompanyNam;
+                jQuery("#geocoder-input").val(newvalue);
+            },
+            onChooseEvent: function () {
+                var index = $("#geocoder-input").getSelectedItemData().CompanyNam;
 
-            $("#geocoder-input").val(index).trigger("change");
+                $("#geocoder-input").val(index).trigger("change");
+            }
         }
-    }
-};
+    };
 
-//event for when the autocomplete is happening
-$('#geocoder-input').easyAutocomplete(options);
+    //event for when the autocomplete is happening
+    $('#geocoder-input').easyAutocomplete(options);
+}
