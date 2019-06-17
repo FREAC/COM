35de4f8842ecc856b04f158b42677bf1aa562f15
Stephen Hodge <shodge@fsu.edu>
2019-06-17 14:15:03 -0400

    new plugins and new picklist for new things we did not anticipate

--- leaflet_app/js/app.js
+++ leaflet_app/js/app.js
@@ -5,7 +5,15 @@ const default_fill_color = "#ED9118";
 const default_outline_color = "#FFFFFF";
 const selected_color = "#2BBED8";
 const selected_fill_opacity = 1;
-
+$('input[name="insurance"]').amsifySuggestags({
+    suggestions: ['Aetna', 'Beacon', 'Beech Street', 'Cigna', 'Coventry','First Health','Healthy Kids',
+    'Magellan','Medicare'
+    ],
+    whiteList: true
+});
+$(function(){
+	$(".mselect").multiselect();
+});
 // TODO: Can this be dealt with using Bootstrap components?
 $(document).ready(function () {
     $('.geocoder-control').click(function () {
